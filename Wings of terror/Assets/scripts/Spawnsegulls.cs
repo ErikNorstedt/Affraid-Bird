501e6e06d4f65196d791cfc92d307ff123391b46
Erik Norstedt <eriknorth.swe@gmail.com>
2020-10-18 11:13:31 +0200

    doing stuff to the prefabs

---
+++ Wings of terror/Assets/scripts/Spawnsegulls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnsegulls : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] SeagullPattern;
+
+    private int rand;
+
+    private float timeBtwSpawn;
+    public float startTimeBtwSpawn;
+    public float decreaceTime;
+    public float minTime = 0.65f;
+
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwSpawn <= 0)
+        {
+            rand = Random.Range(0, SeagullPattern.Length);
+            Instantiate(SeagullPattern[rand], transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+            if(startTimeBtwSpawn > minTime)
+            {
+            startTimeBtwSpawn -= decreaceTime;
+            }
+        }
+        else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+    }
+}
